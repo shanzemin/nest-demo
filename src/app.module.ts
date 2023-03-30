7bd852bd864e431f7356ee6fbd85c682e29de858
shanzemin <nigegeswo520@qq.com>
2023-03-30 18:37:08 +0800

    🦄 refactor:重构

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,18 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { BooksModule } from './books/books.module';
+import { Module } from '@nestjs/common'
+// import { ConfigModule } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose'
+import { UsersModule } from './modules/users/users.module'
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/libraries', {
-      useNewUrlParser: true,
+      useNewUrlParser: true
     }),
-    BooksModule
-],
-  controllers: [AppController],
-  providers: [AppService]
+    UsersModule
+    // ConfigModule.forRoot({})
+  ],
+  controllers: [],
+  providers: []
 })
+
 export class AppModule {}
